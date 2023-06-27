6ac06d0045b55df11df5ecdf6b9bc502825f6626
MikeK21 <mikexkiriakou@gmail.com>
2023-06-26 20:05:17 -0400

    initial commit

---
+++ app/src/main/java/com/example/wguterminator/UI/CourseList.java
@@ -0,0 +1,16 @@
+package com.example.wguterminator.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wguterminator.R;
+
+public class CourseList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_list);
+    }
+}
