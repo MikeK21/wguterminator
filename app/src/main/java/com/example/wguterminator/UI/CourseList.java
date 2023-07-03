fc4055432dda4fa78c0f45945fa04f88186392fd
MikeK21 <mikexkiriakou@gmail.com>
2023-07-03 10:09:17 -0400

    commit WIP

--- app/src/main/java/com/example/wguterminator/UI/CourseList.java
+++ app/src/main/java/com/example/wguterminator/UI/CourseList.java
@@ -2,15 +2,26 @@ package com.example.wguterminator.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.wguterminator.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class CourseList extends AppCompatActivity {
+public class  CourseList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_list);
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CourseList.this,CourseDetails.class);
+                startActivity(intent);
+            }
+        });
     }
 }
