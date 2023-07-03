2809f1c685c1e8650b8a3eabf93a1a9a00687249
MikeK21 <mikexkiriakou@gmail.com>
2023-07-03 16:01:39 -0400

    commit WIP 4.0

---
+++ app/src/main/java/com/example/wguterminator/UI/CourseAdapter.java
@@ -0,0 +1,78 @@
+package com.example.wguterminator.UI;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.wguterminator.Entities.Course;
+import com.example.wguterminator.R;
+
+import java.util.List;
+
+public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseViewHolder> {
+
+    private List<Course> mCourses;
+    private final Context context;
+    private final LayoutInflater mInflater;
+
+    class CourseViewHolder extends RecyclerView.ViewHolder {
+        private final TextView courseItemView;
+        private CourseViewHolder(View itemView) {
+            super(itemView);
+            courseItemView = itemView.findViewById(R.id.textView2);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    final Course current = mCourses.get(position);
+                    Intent intent = new Intent(context, CourseDetails.class);
+                    intent.putExtra("id", current.getCourseId());
+                    intent.putExtra("name", current.getCourseName());
+                    intent.putExtra("price", current.getPrice());
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+
+    public CourseAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public CourseAdapter.CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.course_list_item,parent,false);
+        return new CourseViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourseAdapter.CourseViewHolder holder, int position) {
+        if (mCourses != null) {
+            Course current = mCourses.get(position);
+            String name = current.getCourseName();
+            holder.courseItemView.setText(name);
+        }
+        else {
+            holder.courseItemView.setText("No Course Name");
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCourses.size();
+    }
+
+    public void setCourses(List<Course> courses) {
+        mCourses = courses;
+        notifyDataSetChanged();
+    }
+}
--- app/src/main/java/com/example/wguterminator/UI/CourseList.java
+++ app/src/main/java/com/example/wguterminator/UI/CourseList.java
@@ -1,21 +1,34 @@
 package com.example.wguterminator.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.wguterminator.Database.Repository;
+import com.example.wguterminator.Entities.Course;
 import com.example.wguterminator.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class  CourseList extends AppCompatActivity {
+import java.util.List;
 
+public class  CourseList extends AppCompatActivity {
+    private Repository repository;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_list);
+        RecyclerView recyclerView = findViewById(R.id.courserecyclerview);
+        final CourseAdapter courseAdapter = new CourseAdapter(this);
+        recyclerView.setAdapter(courseAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        repository = new Repository(getApplication());
+        List<Course> allCourses = repository.getmAllCourses();
         FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        courseAdapter.setCourses(allCourses);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
