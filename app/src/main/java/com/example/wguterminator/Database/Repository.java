a780593fcb8d309e52adbd8dbb097868040391d2
MikeK21 <mikexkiriakou@gmail.com>
2023-07-03 14:53:05 -0400

    commit WIP 3

--- app/src/main/java/com/example/wguterminator/Database/Repository.java
+++ app/src/main/java/com/example/wguterminator/Database/Repository.java
@@ -33,7 +33,7 @@ public class Repository {
         mAssessmentDAO = db.assessmentDAO();
     }
 
-    public List<Term> getmALlTerms() {
+    public List<Term> getmAllTerms() {
         databaseExecutor.execute(() ->{
             mAllTerms = mTermDAO.getAllTerms();
         });
@@ -43,5 +43,129 @@ public class Repository {
             e.printStackTrace();
         }
         return mAllTerms;
+    }
+
+    public void insert(Term term) {
+        databaseExecutor.execute(()->{
+            mTermDAO.insert(term);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(Term term) {
+        databaseExecutor.execute(()->{
+            mTermDAO.update(term);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(Term term) {
+        databaseExecutor.execute(()->{
+            mTermDAO.delete(term);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Course> getmAllCourses() {
+        databaseExecutor.execute(() ->{
+            mAllCourses = mCourseDAO.getAllCourses();
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return mAllCourses;
+    }
+
+    public void insert(Course course) {
+        databaseExecutor.execute(()->{
+            mCourseDAO.insert(course);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(Course course) {
+        databaseExecutor.execute(()->{
+            mCourseDAO.update(course);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(Course course) {
+        databaseExecutor.execute(()->{
+            mCourseDAO.delete(course);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public List<Assessment> getmAllAssessments() {
+        databaseExecutor.execute(() ->{
+            mAllAssessments = mAssessmentDAO.getAllAssessments();
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return mAllAssessments;
+    }
+
+    public void insert(Assessment assessment) {
+        databaseExecutor.execute(()->{
+            mAssessmentDAO.insert(assessment);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(Assessment assessment) {
+        databaseExecutor.execute(()->{
+            mAssessmentDAO.update(assessment);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delete(Assessment assessment) {
+        databaseExecutor.execute(()->{
+            mAssessmentDAO.delete(assessment);
+        });
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/example/wguterminator/UI/MainActivity.java
+++ app/src/main/java/com/example/wguterminator/UI/MainActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.wguterminator.Database.Repository;
+import com.example.wguterminator.Entities.Assessment;
+import com.example.wguterminator.Entities.Course;
+import com.example.wguterminator.Entities.Term;
 import com.example.wguterminator.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,5 +29,23 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addSampleData:
+                Term term = new Term(0, "May 2023", "05-01-2023");
+                Repository repository = new Repository(getApplication());
+                repository.insert(term);
+                Course course = new Course(0,"Biology of the Chesapeake", "599.99");
+                repository.insert(course);
+                Assessment assessment = new Assessment(0, "Chesapeake Critters", "742.99");
+                repository.insert(assessment);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
