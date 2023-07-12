f053ffe7ef68a337d5b5f7b3a355ec932fbc0596
MikeK21 <mikexkiriakou@gmail.com>
2023-07-12 08:51:00 -0400

    commit WIP 5.0

--- app/src/main/java/com/example/wguterminator/UI/CourseDetails.java
+++ app/src/main/java/com/example/wguterminator/UI/CourseDetails.java
@@ -3,14 +3,20 @@ package com.example.wguterminator.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.wguterminator.R;
 
 public class CourseDetails extends AppCompatActivity {
 
+    EditText editName;
+    EditText editDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_details);
+        editName = findViewById(R.id.courseName);
+        editDate = findViewById(R.id.courseDate);
     }
 }
--- app/src/main/java/com/example/wguterminator/UI/TermDetails.java
+++ app/src/main/java/com/example/wguterminator/UI/TermDetails.java
@@ -5,16 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.wguterminator.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TermDetails extends AppCompatActivity {
 
+    EditText editName;
+    EditText editDate;
+    String name;
+    String date;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_details);
+        editDate = findViewById(R.id.termDate);
+        editName = findViewById(R.id.termName);
+        name = getIntent().getStringExtra("name");
+        date = getIntent().getStringExtra("date");
+        editName.setText(name);
+        editDate.setText(date);
+
+
         FloatingActionButton fab = findViewById(R.id.floatingActionButton3);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
